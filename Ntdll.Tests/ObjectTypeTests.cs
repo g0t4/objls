75c79b7c9cde8abea38c179acf0cae73985d89a8
Wes Higbee <wes.mcclure@gmail.com>
2016-12-04 14:11:52 -0800

    Add test to get type of an object that isn't a directory

--- Ntdll.Tests/ObjectTypeTests.cs
+++ Ntdll.Tests/ObjectTypeTests.cs
@@ -13,5 +13,15 @@ namespace Ntdll.Tests
 
 			Assert.Equal(type, "Directory");
 		}
+
+		[Fact]
+		public void GetObjectType_IsNotADirectory_ReturnsOtherwise()
+		{
+			var directory = @"\GLOBAL??\C:";
+
+			var type = NtdllHelper.GetObjectType(directory);
+
+			Assert.Equal(type, "NOT");
+		}
 	}
 }

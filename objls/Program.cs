d45f1344a235b9cedad039167ddeb50234ee685b
Wes Higbee <wes.mcclure@gmail.com>
2016-12-04 15:30:33 -0800

    Show directory contents when the object is a directory

--- objls/Program.cs
+++ objls/Program.cs
@@ -1,34 +1,47 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
+﻿using System.Linq;
 using PInvoke.Ntdll;
 using static System.Console;
 
-class Program
+internal class Program
 {
-	static void Main(string[] args)
+	private static void Main(string[] args)
 	{
-		var objectName = @"\GLOBAL??\C:";
-		var type = NtdllHelper.GetObjectType(objectName);
-
-		WriteLine(objectName);
-		WriteLine(type);
-		return;
-
-		var objects = NtdllHelper.QueryDirectoryObjects(objectName);
-		WriteLine($"Object Directory of {objectName}");
-		WriteLine();
-		var maxTypeNameLength = objects.Max(o => o.TypeName.Length);
-		foreach (var @object in objects
-					.OrderByDescending(o => o.IsDirectory())
-					.ThenBy(o => o.Name))
+		// default to root object namespace
+		var objectName = @"\";
+		if (args.Length > 0)
 		{
-			WriteLine("{0,-" + maxTypeNameLength + "} {1}", @object.TypeName, @object.Name);
+			objectName = args[0];
+		}
+
+		var type = NtdllHelper.GetObjectType(objectName);
+		WriteLine($"Object type {type}: {objectName}");
+
+		switch (type)
+		{
+			case "Directory":
+				ListDirectoryContents(objectName);
+				break;
 		}
 	}
 
-	
+	private static void ListDirectoryContents(string objectName)
+	{
 
+		WriteLine();
+		WriteLine($"Directory contents:");
+		WriteLine();
+
+		var objects = NtdllHelper.QueryDirectoryObjects(objectName);
+
+		var typeColumnLength = objects.Max(o => o.TypeName.Length) + 1;
+
+		var sortedObjects = objects
+			.OrderByDescending(o => o.IsDirectory())
+			.ThenBy(o => o.Name);
+
+		foreach (var @object in sortedObjects)
+		{
+			WriteLine("{0,-" + typeColumnLength + "} {1}", @object.TypeName, @object.Name);
+		}
+	}
 }
